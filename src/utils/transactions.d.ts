c8e2b28426a8989775ba964bd9d1f84ddf014bbf
Rawallon Cardoso <rawallon@gmail.com>
2023-11-30 23:51:32 -0300

    chore: Add transaction JSON

---
+++ src/utils/transactions.d.ts
@@ -0,0 +1,11 @@
+export interface Transaction {
+  date: number;
+  amount: string;
+  transaction_type: "deposit" | "withdraw";
+  currency: string;
+  account: string;
+  industry: string;
+  state: string;
+}
+
+export type Transactions = Transaction[];
