00bd7e4c4f0f20fe0edff492e81fe2e169e6d11a
Rawallon Cardoso <rawallon@gmail.com>
2023-12-01 00:06:52 -0300

    Feat: Implemented date filtering for transactions

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,64 @@
 "use client";
 
+import { useEffect, useState } from "react";
 
+import DashboardTopBar from "@/component/dashboard/DashboardTopBar";
 import LeftSidebar from "@/component/LeftSidebar";
+import useLocalStorage from "@/hooks/useLocalStorage";
+import useDebounce from "@/utils/debounce";
+import { Transactions } from "@/utils/transactions";
+import transactionsJSON from "@/utils/transactions.json";
+
+const transactionsData = transactionsJSON as Transactions;
+
 
 
 function Dashboard() {
+
+  const getOneWeekAgo = () => {
+    const today = new Date();
+    const oneWeekAgo = new Date(today);
+    oneWeekAgo.setDate(today.getDate() - 7);
+    return oneWeekAgo;
+  };
+  const [storedDashboardPeriod, setStoredDashboardPeriod] = useLocalStorage("selectedDates", {
+    startDate: getOneWeekAgo(),
+    endDate: new Date(),
+  });
+
+
+  const [dashboardPeriod, setDashboardPeriod] = useState<{
+    startDate: Date;
+    endDate: Date;
+  }>({
+    startDate: new Date(storedDashboardPeriod["startDate"]),
+    endDate: new Date(storedDashboardPeriod["endDate"]),
+  });
+
+  const debouncedDateSelection = useDebounce(dashboardPeriod);
+
+  const [transactionsDateFiltered, setTransactionsDateFiltered] =
+    useState<Transactions>(
+      transactionsData.filter(
+        (transactions) =>
+          transactions.date > dashboardPeriod["startDate"].getTime() &&
+          transactions.date < dashboardPeriod["endDate"].getTime()
+      )
+    );
+  useEffect(() => {
+    if (debouncedDateSelection) {
+      const dateFiltered = transactionsData.filter(
+        (transactions) =>
+          transactions.date > dashboardPeriod["startDate"].getTime() &&
+          transactions.date < dashboardPeriod["endDate"].getTime()
+      );
+      setStoredDashboardPeriod({
+        startDate: dashboardPeriod["startDate"],
+        endDate: dashboardPeriod["endDate"],
+      });
+      setTransactionsDateFiltered(dateFiltered);
+    }
+  }, [debouncedDateSelection]);
 
   return (
     <div className="drawer ">
@@ -37,11 +91,13 @@ function Dashboard() {
             </label>
           </div>
           <div className="ml-auto">
-
+          <DashboardTopBar
+              dashboardPeriod={dashboardPeriod}
+              updateDashboardPeriod={setDashboardPeriod}
+            />
           </div>
         </div>
         <main className="flex-1 overflow-y-auto pt-8 px-6  bg-base-200">
-
         </main>
       </div>
       <LeftSidebar />
