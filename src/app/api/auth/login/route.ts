8c55a227346ec28bb841115674d34d70c862cf1a
Rawallon Cardoso <rawallon@gmail.com>
2023-12-01 01:09:14 -0300

    Chore: added Option to fix CORS

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -6,6 +6,22 @@ import { COOKIE_NAME } from "@/constants";
 
 const MAX_AGE = 60 * 60 * 24 * 30; // days;
 
+export async function OPTIONS(request: Request) {
+  const allowedOrigin = request.headers.get("origin");
+  const response = new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": allowedOrigin || "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+
+  return response;
+}
+
 export async function POST(request: Request) {
   const body = await request.json();
 
